05746104c8373e64828bc466f883c22368a19695
KunalSonz <70892432+KunalSonz@users.noreply.github.com>
2021-01-12 12:08:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  dots:true,
+  autoplay:true,
+  autoplayTimeout:1000,
+  autoplayHoverPause:true,
+  responsive:{
+      0:{
+          items:3
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:3
+      },
+      2000:{
+        items:3
+    }
+  }
+})
+
+
